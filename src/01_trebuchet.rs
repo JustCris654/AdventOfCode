bbf07f14f6e443382454def2768f500ee3b0ee44
JustCris654 <cristian.scapin654@icloud.com>
2023-12-03 10:17:29 +0100

    day 1 part 1 refactor

--- src/01_trebuchet.rs
+++ src/01_trebuchet.rs
@@ -3,26 +3,19 @@ use std::{char, fs::read_to_string};
 fn main() {
     let file_path = "input.txt";
 
-    let mut sum = 0;
-
-    let content: Vec<String> = read_to_string(file_path)
+    let sum: u32 = read_to_string(file_path)
         .expect("Cannot read file")
         .lines()
         .map(String::from)
-        .collect();
-
-    for line in content {
-        let nums: Vec<char> = line.chars().filter(|&c| c >= '0' && c <= '9').collect();
-        if nums.len() == 1 {
-            let num = nums.get(0).unwrap().to_digit(10).unwrap() * 11;
-            sum += num;
-        } else if nums.len() > 1 {
-            let num = String::from_iter([nums[0], nums[nums.len() - 1]])
+        .map(|line| line.chars().filter(|&c| c >= '0' && c <= '9').collect())
+        .map(|nums: Vec<char>| match nums.len() {
+            0 => 0,
+            1 => nums.get(0).unwrap().to_digit(10).unwrap() * 11,
+            _ => String::from_iter([nums[0], nums[nums.len() - 1]])
                 .parse::<u32>()
-                .unwrap();
-            sum += num;
-        }
-    }
+                .unwrap(),
+        })
+        .sum();
 
     println!("{}", sum);
 }
